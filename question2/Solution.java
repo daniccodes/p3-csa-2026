88a5e83087cd724b8bd1ec9d280bdff3d5e30190
ojihouihu <dannyc134@nycstudents.net>
2026-05-21 10:01:11 -0400

    Update Solution.java

--- question2/Solution.java
+++ question2/Solution.java
@@ -1,1 +1,16 @@
+Public class Bottle{
+  private double intCap;
+  private double amt;
 
+  Public Water(double cap){
+    intCapacity = cap;
+    amt = cap;
+  }
+
+  public double updateAmount(double removed){
+    if(amt -= removed <= 0.25(IntCap){
+      amt = intCap;
+    }
+    return amt;
+  }
+}
