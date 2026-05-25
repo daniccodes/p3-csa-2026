e72af316e23c1cf29b13a1fc37aae02c98344b68
ojihouihu <dannyc134@nycstudents.net>
2026-05-25 02:19:29 -0400

    Update SolutionPartA.java

--- question1/SolutionPartA.java
+++ question1/SolutionPartA.java
@@ -2,6 +2,7 @@
  * Creates a new account with a unique username.
  * Takes requestedName and appends numbers (1, 2, 3...) until an available name is found.
  */
+
 public Account(String requestedName){
 
   String username = requestedName;
