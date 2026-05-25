1778febcace74d77d6f12d6f329a5965e6c81d7f
ojihouihu <dannyc134@nycstudents.net>
2026-05-25 02:16:18 -0400

    Update Solution.java
    Added comments for better readability.

--- question3/Solution.java
+++ question3/Solution.java
@@ -1,7 +1,13 @@
+/**
+ * Returns the count of students who have more absences in history than in math.
+ * Compares students via student ID.
+*/
+
+
 public int moreHistoryThanMathAbsences(){
   int count = 0;
-  for(int i = 0; i< historyList.size(); i++){
-    for(int g = 0; g<mathList.size(); g++){
+  for(int i = 0; i < historyList.size(); i++){
+    for(int g = 0; g < mathList.size(); g++){
       if(historyList.get(i).getStudentID() == mathList.get(g).getStudentID()){
         if(historyList.get(i).getAbsences() > mathList.get(g).getAbsences()){
           count++;
