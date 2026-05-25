8952033a4c75020902f852481d35a3d07f865ab7
ojihouihu <dannyc134@nycstudents.net>
2026-05-25 01:25:53 -0400

    Update Solution.java
    Added solution to frq3 : moreHistoryThanMathAbsences()

--- question3/Solution.java
+++ question3/Solution.java
@@ -1,1 +1,13 @@
-
+public int moreHistoryThanMathAbsences(){
+  int count = 0;
+  for(int i = 0; i< historyList.size(); i++){
+    for(int g = 0; g<mathList.size(); g++){
+      if(historyList.get(i).getStudentID() == mathList.get(g).getStudentID()){
+        if(historyList.get(i).getAbsences() > mathList.get(g).getAbsences()){
+          count++;
+        }
+      }
+    }
+  }
+  return count;
+}
