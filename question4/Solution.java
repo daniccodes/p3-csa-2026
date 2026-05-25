f304a889d8f877b5f1c40d753a4e33508b920629
ojihouihu <dannyc134@nycstudents.net>
2026-05-25 01:35:56 -0400

    Update Solution.java
    Added solution to frq4: getPointsForRow(int targetRow)

--- question4/Solution.java
+++ question4/Solution.java
@@ -1,1 +1,17 @@
+public int  getPointsForRow(int targetRow){
+  int sum = 0;
+  boolean same = true;
+  for(int i = 0; i< board[targetRow].length;i++){
+    sum += board[targetROw][i].getPoints();
 
+    if(!board[targetRow][i].getColor().equals(board[targetRow][0].getColor())){
+      same = false;
+    }
+  }
+  if(same){
+    return sum*2
+  }
+  else{
+    return sum;
+  }
+}
