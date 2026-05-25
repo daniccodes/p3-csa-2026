f0fde4375506a8f0fb31d1b3d9617ebe73a76870
ojihouihu <dannyc134@nycstudents.net>
2026-05-25 02:01:04 -0400

    Update SolutionPartA.java
    FIXED punctuality and spelling

--- question1/SolutionPartA.java
+++ question1/SolutionPartA.java
@@ -1,6 +1,6 @@
 /**
  * Creates a new account with a unique username.
- *Takes requestedName and appends numbers(1, 2, 3...) until an available name is found
+ * Takes requestedName and appends numbers (1, 2, 3...) until an available name is found.
  */
 public Account(String requestedName){
 
