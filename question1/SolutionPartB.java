1d1b80790e1e6d7c13df07b775498cbb94e2cf3d
ojihouihu <dannyc134@nycstudents.net>
2026-05-25 02:06:18 -0400

    Update SolutionPartB.java
    Added comments for context.

--- question1/SolutionPartB.java
+++ question1/SolutionPartB.java
@@ -1,11 +1,15 @@
+/**
+ * Returns the username with hyphens and the character before each hyphen removed.
+ */
+
 public String getShortenedName(){
   String result = "";
-  for(int i = 0; i< username.length();i++){
-    if username.substring(i,i+1).equals("-")){
-      result = result.substring(0,result.length()-1)';
+  for(int i = 0; i < username.length(); i++){
+    if (username.substring(i, i + 1).equals("-")){
+      result = result.substring(0, result.length() -1);
     }
     else{
-      result += username.substring(i,i+1);
+      result += username.substring(i, i + 1);
     }
       
   }
