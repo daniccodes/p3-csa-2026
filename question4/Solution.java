1d6aefd55f1cac2d69fb223f1c60f6d44482d4c3
ojihouihu <dannyc134@nycstudents.net>
2026-05-25 02:19:02 -0400

    Update Solution.java
    Added comments for better readability.

--- question4/Solution.java
+++ question4/Solution.java
@@ -1,15 +1,21 @@
+/**
+ * Calculates points for "targetRow".
+ * Returns double points if all spaces in "targetRow" are the same color.
+*/
+
 public int  getPointsForRow(int targetRow){
   int sum = 0;
   boolean same = true;
-  for(int i = 0; i< board[targetRow].length;i++){
-    sum += board[targetROw][i].getPoints();
+
+  for(int i = 0; i < board[targetRow].length; i++){
+    sum += board[targetRow][i].getPoints();
 
     if(!board[targetRow][i].getColor().equals(board[targetRow][0].getColor())){
       same = false;
     }
   }
   if(same){
-    return sum*2
+    return sum * 2
   }
   else{
     return sum;
